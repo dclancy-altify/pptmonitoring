bef4634c8c7fabc7ebbacd4ef3a3205f8a2bc724
Dan Clancy <dclancy@thetasgroup.com>
2018-05-30 16:24:25 +0100

    test

--- pp_gen_init.bat
+++ pp_gen_init.bat
@@ -1,2 +1,7 @@
-﻿rmdir /s /q C:\temp\dev
+﻿del C:\temp\dev\8.36\accountmap\ppt\*.*x
+del C:\temp\dev\8.36\insightmap\ppt\*.*x
+del C:\temp\dev\8.36\om\ppt\*.*x
+del C:\temp\dev\8.36\om\doc\*.*x
+del C:\temp\dev\8.36\am\ppt\*.*x
+del C:\temp\dev\8.36\am\doc\*.*x
 del c:\temp\results.zip
